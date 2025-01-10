3452d772c5a0d0766e39e244a97f6ea530f6d081
Ram Nath <yogiram727@gmail.com>
2025-01-10 18:59:04 +0545

    done for today

--- A_Advanced_DSA_Solutions/A_Arrays/Practice.java
+++ A_Advanced_DSA_Solutions/A_Arrays/Practice.java
@@ -1,33 +1,22 @@
 package A_Advanced_DSA_Solutions.A_Arrays;
 
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Practice {
-    public void findMinMax(int arr[]) {
-        int min = arr[0];
-        int max = arr[0];
-
+    public void maxSubSum(int arr[]) {
+        int currentSum = arr[0];
+        int maxSum = arr[0];
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < min) {
-                min = arr[i];
-            }
-            if (arr[i] > max) {
-                max = arr[i];
-            }
+            currentSum = Math.max(arr[i], arr[i] + currentSum);
+            maxSum = Math.max(currentSum, maxSum);
         }
-
-        System.out.println();
-        System.out.println("Maximum Element in the array is " + max);
-        System.out.println();
-        System.out.println("Minimum Element in the array is: " + min);
-        System.out.println();
+        System.out.println("Max Sum of the Arrays is: " + maxSum);
     }
 
-    public static void main(String[] args) {
-        Practice find = new Practice();
-        int arr[] = { 2, 4, 5, 6, 34, 544, 6666, 3, 1 };
-        System.out.println("Given Array: " + Arrays.toString(arr));
-        find.findMinMax(arr);
+    public static void main(String args[]) {
+        Practice p = new Practice();
+        int[] arr = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+        System.out.println("Given Array is : " + Arrays.toString(arr));
+        p.maxSubSum(arr);
     }
 }
