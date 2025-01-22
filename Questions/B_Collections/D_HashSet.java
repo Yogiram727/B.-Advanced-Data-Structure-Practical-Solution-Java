f74a4171c5e6df3375967fe18af78f204120b818
Ram Nath <yogiram727@gmail.com>
2025-01-22 16:25:32 +0545

    hashset added

---
+++ Questions/B_Collections/D_HashSet.java
@@ -0,0 +1,20 @@
+package Questions.B_Collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class D_HashSet {
+    public static void main(String[] args) {
+        HashSet<Integer> hs = new HashSet<>();
+        hs.add(10);
+        hs.add(20);
+        hs.add(10);
+        hs.add(45);
+
+        Iterator<Integer> itr = hs.iterator();
+        while (itr.hasNext()) {
+            System.out.println(itr.next());
+        }
+    }
+
+}
