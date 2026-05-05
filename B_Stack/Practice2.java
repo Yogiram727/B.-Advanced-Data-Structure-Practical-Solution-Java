2b7831761e17969a23c13f0cd8c208f3a1025d6e
Ram Nath <yogiram727@gmail.com>
2026-05-05 15:02:30 +0545

    stack practice

---
+++ B_Stack/Practice2.java
@@ -0,0 +1,56 @@
+public class Practice2 {
+    
+    int n=5;
+    int[]stack=new int[n];
+    int top=-1;
+
+    public void push(int value){
+        if(top==n-1){
+            System.out.println("Stack Overflow");
+            return;
+        }
+        top++;
+        stack[top]=value;
+    }
+
+
+    public void pop(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+            return;
+        }
+        top--;
+    }
+
+    public void display(){
+        if(top==-1){
+            System.out.println("Stack is empty");
+            return;
+        }
+        for(int i=top;i>=0;i--){
+            System.out.println(stack[i]);
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Practice2 stk=new Practice2();
+        stk.push(1);
+        stk.push(3);
+        stk.push(5);
+        stk.push(7);
+        stk.push(9);
+        System.out.println("Given Stack: ");
+        stk.display();
+        System.out.println("Stack after partial deletion: ");
+        stk.pop();
+        stk.pop();
+        stk.display();
+        System.out.println("Stack after full deletion: ");
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        stk.display();
+        System.out.println();
+    }
+}
---
+++ B_Stack/Practice3.java
@@ -0,0 +1,63 @@
+public class Practice3 {
+    
+    public static class Node{
+        int data;
+        Node next;
+        Node(int value){
+            this.data=value;
+            this.next=null;
+        }
+    }
+
+    Node top=null;
+
+    public void push(int value){
+        Node newNode=new Node(value);
+        newNode.next=top;
+        top=newNode;
+    }
+
+    public void pop(){
+        if(top==null){
+            System.out.println("Stack is empty");
+            return;
+        }
+        top=top.next;
+    }
+
+    public void display(){
+        if(top==null){
+            System.out.println("Stack is empty");
+            return;
+        }
+        Node ptr=top;
+        while(ptr!=null){
+            System.out.println(ptr.data);
+            ptr=ptr.next;
+        }
+        System.out.println();
+        
+    }
+
+
+    public static void main(String[] args) {
+        Practice3 stk=new Practice3();
+        stk.push(1);
+        stk.push(2);
+        stk.push(3);
+        stk.push(4);
+        stk.push(5);
+        System.out.println("Given Stack: ");
+        stk.display();
+        System.out.println("Stack after partial deletion: ");
+        stk.pop();
+        stk.pop();
+        stk.display();
+        System.out.println("Stack after full deletion: ");
+        stk.pop();
+        stk.pop();
+        stk.pop();
+        stk.display();
+        System.out.println();
+    }
+}
